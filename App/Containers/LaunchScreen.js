f504c0fde39643489b8b29ef866e741227058210
bayarb <bogdanov@bizapp.ru>
2018-04-06 15:59:47 +0800

    add map and not working geolocation

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -1,29 +1,110 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, Image, View } from 'react-native'
+import { View, Dimensions, PermissionsAndroid, Platform } from 'react-native'
 import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/LaunchScreenStyles'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import NightMapStyle from './Styles/NightMapStyle.json'
+
+let { width, height } = Dimensions.get('window')
+const ASPECT_RATIO = width / height
+const LATITUDE = 0
+const LONGITUDE = 0
+const LATITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 export default class LaunchScreen extends Component {
+
+  constructor () {
+    super()
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+    }
+  }
+
+  requestAccessFineLocation () {
+    try {
+      const granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          'title': 'Cool Photo App Camera Permission',
+          'message': 'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.'
+        }
+      )
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera')
+        this.getLocation()
+      } else {
+        console.log('Camera permission denied')
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+
+  getLocation () {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          }
+        })
+      },
+      (error) => console.log(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    )
+    this.watchID = navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          region: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }
+        })
+      }
+    )
+  }
+
+  componentDidMount () {
+    if (Platform.OS === 'ios') {
+      this.getLocation()
+    } else {
+      this.requestAccessFineLocation()
+    }
+  }
+
+  componentWillUnmount () {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
   render () {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
-
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
-
-        </ScrollView>
-      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.container}
+        customMapStyle={NightMapStyle}
+        showsUserLocation
+        region={this.state.region}
+        onRegionChange={region => this.setState({region})}
+        onRegionChangeComplete={region => this.setState({region})}
+      >
+        <MapView.Marker
+          coordinate={this.state.region}
+        />
+      </MapView>
     )
   }
 }
--- ios/MapIgniteExample/AppDelegate.m
+++ ios/MapIgniteExample/AppDelegate.m
@@ -11,12 +11,17 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps;
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+  
+  [GMSServices provideAPIKey:@"AIzaSyBaxD-2xvvKCBVZsYRvl5ssagGdJsEqb2c"];
+  
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
